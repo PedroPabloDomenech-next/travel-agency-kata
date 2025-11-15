aa7280b9dc2273dd95b981308f1944fe4211944a
daniirivv <danielriveranunezdearenas@outlook.com>
2025-11-15 20:14:02 +0100

    Add Strategy Pattern in BehavioralPatternExercices.java

--- src/test/java/com/breadhardit/travelagencykata/BehavioralPatternExercices.java
+++ src/test/java/com/breadhardit/travelagencykata/BehavioralPatternExercices.java
@@ -1,6 +1,5 @@
 package com.breadhardit.travelagencykata;
 
-import com.breadhardit.travelagencykata.domain.Customer;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -8,94 +7,30 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Collectors;
+
+import static com.breadhardit.travelagencykata.BehavioralPatternExercices.Travel.SCHENGEN_COUNTRIES;
 
 @Slf4j
 public class BehavioralPatternExercices {
-    /* EXERCISE 1
-        Travels has an origin and a destination. Travels have some restrictions:
-        - Travels with origin and destination in the same country require only Identity Document
-        - Travels with origin and destination in schengen space, requires Passport
-        - Travels with origin or destination out of schengen space, requires Visa
-     */
-    @Data
-    public static class Travel {
-        public static final List<String> SCHENGEN_COUNTRIES = List.of("Spain","France","Iceland","Italy","Portugal");
-        String id;
-        String name;
-        String origin;
-        String destination;
-        Boolean sameCountryTravel = Boolean.FALSE;
-        Boolean schengenSpaceTravel = Boolean.FALSE;
-        Boolean visaRequiredTravel = Boolean.FALSE;
-        public Travel(String id,String name,String origin,String destination) {
-            if (origin.equals(destination)) this.sameCountryTravel = Boolean.TRUE;
-            else if (SCHENGEN_COUNTRIES.contains(origin) && SCHENGEN_COUNTRIES.contains(destination)) this.schengenSpaceTravel = Boolean.TRUE;
-            else this.visaRequiredTravel = Boolean.TRUE;
-        }
+    public static boolean isInSchengenSpace(String country) {
+        return SCHENGEN_COUNTRIES.contains(country);
     }
-    public static void scanVisa() {
-        log.info("Applying visa...");
-    }
-    public static void scanDNI() {
-        log.info("Applying DNI...");
-    }
-    public static void scanPassport() {
-        log.info("Applying Passport");
-    }
+
     @Test
-    // When customer buy a new Travel we have to scan the proper documentation
-    public void travelAgency() {
+        // When customer buy a new Travel we have to scan the proper documentation
+    void travelAgency() {
         List<Travel> travels = List.of(
-                new Travel(UUID.randomUUID().toString(),"PYRAMIDS TOUR","Spain","EGYPT"),
-                new Travel(UUID.randomUUID().toString(),"LISBOA TOUR","Spain","Portugal"),
-                new Travel(UUID.randomUUID().toString(),"LISBOA TOUR","Portugal","Portugal")
+                new Travel(UUID.randomUUID().toString(), "PYRAMIDS TOUR", "Spain", "EGYPT"),
+                new Travel(UUID.randomUUID().toString(), "LISBOA TOUR", "Spain", "Portugal"),
+                new Travel(UUID.randomUUID().toString(), "LISBOA TOUR", "Portugal", "Portugal")
         );
-        for (Travel travel: travels) {
+        for (Travel travel : travels) {
             if (travel.visaRequiredTravel) scanVisa();
             else if (travel.schengenSpaceTravel) scanPassport();
             else if (travel.sameCountryTravel) scanDNI();
         }
     }
-    // Refactor code using the proper structural pattern
 
-
-    /*
-     * When a new employee is enrolled, company sends a greetins e-mail.
-     * A notification service is querying the database every second looking for new employees to notify
-     */
-    @Builder
-    @Data
-    public static class Employee {
-        final String id;
-        final String name;
-        final String email;
-        @Builder.Default
-        Boolean greetingDone = Boolean.FALSE;
-    }
-    public static class EmployeesRepository{
-        private static final ConcurrentHashMap<String,Employee> EMPLOYEES = new ConcurrentHashMap<>();
-        public void addEmployee(Employee employee) {
-            EMPLOYEES.put(employee.getId(),employee);
-        }
-        public List<Employee> getUnnotifiedEmployees() {
-            return EMPLOYEES.values().stream().filter(e -> !e.greetingDone).toList();
-        }
-    }
-    @Value
-    @AllArgsConstructor
-    public static class GreetingsNotificator {
-        EmployeesRepository employeesRepository;
-        @SneakyThrows
-        public void applyNotifications() {
-            while (true) {
-                log.info("Aplying notifications");
-                List<Employee> employeesToNotify = employeesRepository.getUnnotifiedEmployees();
-                employeesToNotify.forEach(e -> {log.info("Notifying {}", e);e.setGreetingDone(Boolean.TRUE);});
-                Thread.sleep(100);
-            }
-        }
-    }
     @Test
     @SneakyThrows
     public void companyTest() {
@@ -106,6 +41,133 @@ public class BehavioralPatternExercices {
         employeesRepository.addEmployee(Employee.builder().id("1").name("Pepe").email("pepe@pepemail.com").build());
         Thread.sleep(200);
         employeesRepository.addEmployee(Employee.builder().id("2").name("Juan").email("pepe@pepemail.com").build());
+    }
+
+    /* EXERCISE 1
+        Travels has an origin and a destination. Travels have some restrictions:
+        - Travels with origin and destination in the same country require only Identity Document
+        - Travels with origin and destination in schengen space, requires Passport
+        - Travels with origin or destination out of schengen space, requires Visa
+     */
+    @Data
+    public abstract static class Travel {
+        public static final List<String> SCHENGEN_COUNTRIES = List.of("Spain", "France", "Iceland", "Italy",
+                "Portugal");
+        String id;
+        String name;
+        String origin;
+        String destination;
+
+        public Travel(String name, String origin, String destination) {
+            this.id = UUID.randomUUID().toString();
+            this.name = name;
+            this.origin = origin;
+            this.destination = destination;
+        }
+
+        public Travel(String origin, String destination) {
+            this("", origin, destination);
+        }
+
+        abstract void scanDocumentation();
+    }
+
+    public static class LocalTravel extends Travel {
+
+        public LocalTravel(String origin, String destination) {
+            super(origin, destination);
+        }
+
+        @Override
+        public void scanDocumentation() {
+            log.info("Scanning National Identifier Document...");
+        }
+    }
+
+    public static class SchengenSpaceTravel extends Travel {
+
+        public SchengenSpaceTravel(String origin, String destination) {
+            super(origin, destination);
+        }
+
+        @Override
+        public void scanDocumentation() {
+            log.info("Scanning Passport...");
+        }
+    }
+
+    public static class InternationalTravel extends Travel {
+
+        public InternationalTravel(String origin, String destination) {
+            super(origin, destination);
+        }
+
+        @Override
+        public void scanDocumentation() {
+            log.info("Scanning VISA...");
+        }
+    }
+
+    public static class TravelFactory {
+        /**
+         * PROBLEMA: Utilizar un String o un Enum que crece al agregar un tipo nuevo viola el OCP
+         * SOLUCIÓN: Registro dinámico (investigar)
+         */
+        public static Travel createTravel(String origin, String destination) {
+            if (origin.equals(destination))
+                return new LocalTravel(origin, destination);
+            else if (isInSchengenSpace(origin) && isInSchengenSpace(destination))
+                return new SchengenSpaceTravel(origin, destination);
+            else if (!isInSchengenSpace(origin) || !isInSchengenSpace(destination))
+                return new InternationalTravel(origin, destination);
+            else throw new IllegalArgumentException("Invalid Travel arguments");
+        }
+    }
+
+    /*
+     * When a new employee is enrolled, company sends a greetins e-mail.
+     * A notification service is querying the database every second looking for new employees to notify
+     */
+
+    @Builder
+    @Data
+    public static class Employee {
+        final String id;
+        final String name;
+        final String email;
+        @Builder.Default
+        Boolean greetingDone = Boolean.FALSE;
+    }
+
+    public static class EmployeesRepository {
+        private static final ConcurrentHashMap<String, Employee> EMPLOYEES = new ConcurrentHashMap<>();
+
+        public void addEmployee(Employee employee) {
+            EMPLOYEES.put(employee.getId(), employee);
+        }
+
+        public List<Employee> getUnnotifiedEmployees() {
+            return EMPLOYEES.values().stream().filter(e -> !e.greetingDone).toList();
+        }
+    }
+
+    @Value
+    @AllArgsConstructor
+    public static class GreetingsNotificator {
+        EmployeesRepository employeesRepository;
+
+        @SneakyThrows
+        public void applyNotifications() {
+            while (true) {
+                log.info("Aplying notifications");
+                List<Employee> employeesToNotify = employeesRepository.getUnnotifiedEmployees();
+                employeesToNotify.forEach(e -> {
+                    log.info("Notifying {}", e);
+                    e.setGreetingDone(Boolean.TRUE);
+                });
+                Thread.sleep(100);
+            }
+        }
     }
     // Use the proper behavioral pattern to avoid the continuous querying to database
 }
--- src/test/java/com/breadhardit/travelagencykata/CreationalPatternExercices.java
+++ src/test/java/com/breadhardit/travelagencykata/CreationalPatternExercices.java
@@ -6,52 +6,180 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
-public class CreationalPatternExercices {
+class CreationalPatternExercices {
     /*
      * Banking accounts has movements. And each movement can be a deposit or withdrawal
      * After a few months operating, we need to create new types of movements:
-     *   - TRANSFER: It's a withdrawal, bet we need the destination account number
+     *   - TRANSFER: It's a withdrawal, but we need the destination account number
      *   - ANNULMENT: It cancels a movement, so, we need the original movement
      */
+
     @Data
     @Builder
-    public static class Movement {
-        public enum MovementType {DEPOSIT,WITHDRAWAL}
-        String id;
-        MovementType type;
-        Long amount;
-        String description;
+    /*
+     Las clases abstractas se consideran incompletas por defecto; puede implementar la interfaz sin definir todos
+     sus métodos, siempre y cuando sus clases hijas sí lo hagan. Por tanto, solo implementa el getId() y delega la
+     implementación de apply() y revert() a sus clases hija.
+     (De hecho, IntellIJ menciona esto en la alerta cuando declaras la clase como concreta en lugar de abstracta).
+    */
+    public abstract static class Movement{
+        private final String id;
+        private final String description;
+
+        abstract void apply(Account account);
+        abstract void revert(Account account);
     }
+
+    public static class DepositMovement extends Movement  {
+        private final Long amount;
+
+        private DepositMovement(String id, String description, Long amount) {
+            super(id, description);
+            this.amount = amount;
+        }
+
+        private DepositMovement(Long amount) {
+            this(UUID.randomUUID().toString(), "", amount);
+        }
+
+        @Override
+        public void apply(Account account){
+            account.balance += amount;
+        }
+
+        @Override
+        public void revert(Account account){
+            new WithdrawalMovement(UUID.randomUUID().toString(), "", amount).apply(account);
+        }
+    }
+
+    public static class WithdrawalMovement extends Movement  {
+        private final Long amount;
+
+        private WithdrawalMovement(String id, String description, Long amount) {
+            super(id, description);
+            this.amount = amount;
+        }
+
+        private WithdrawalMovement(Long amount) {
+            this(UUID.randomUUID().toString(), "", amount);
+        }
+
+        @Override
+        public void apply(Account account){
+            account.balance -= amount;
+        }
+
+        @Override
+        public void revert(Account account){
+            new DepositMovement(UUID.randomUUID().toString(), "", amount).apply(account);
+        }
+    }
+
+    public static class TransferMovement extends Movement {
+        private final Account destination;
+        private final Long amount;
+
+        private TransferMovement(String id, String description, Account destination, Long amount) {
+            super(id, description);
+            this.destination = destination;
+            this.amount = amount;
+        }
+
+        private TransferMovement(Account destination, Long amount) {
+            this(UUID.randomUUID().toString(), "Transfer to " + destination, destination, amount);
+        }
+
+        @Override
+        public void apply(Account source){
+            new WithdrawalMovement(this.amount).apply(source);
+            new DepositMovement(this.amount).apply(this.destination);
+        }
+
+        @Override
+        public void revert(Account source){
+            /*
+             Preguntar si es correcto esto: realiza una transferencia desde una cuenta que no es tuya solo para
+             devolverte el dinero de la transferencia que quieres deshacer...
+            */
+            new TransferMovement(
+                    UUID.randomUUID().toString(),
+                        "Revert transfer to " + this.destination,
+                    source,
+                    this.amount
+            ).apply(destination);
+        }
+    }
+
+    public static class Annulment extends Movement {
+
+        private final Movement movementToRevert;
+
+        private Annulment(String id, String description, Movement movementToRevert) {
+            super(id, description);
+            this.movementToRevert = movementToRevert;
+        }
+
+        private Annulment(Movement movementToRevert) {
+            this(UUID.randomUUID().toString(), "", movementToRevert);
+        }
+
+        @Override
+        public void apply(Account account){
+            this.movementToRevert.revert(account);
+        }
+
+        @Override
+        public void revert(Account account){
+            this.movementToRevert.apply(account);
+        }
+    }
+
+    public static class MovementFactory{
+        /**
+         * PROBLEMA: La factoría recibe parámetros que no siempre van a usarse, puesto que cada tipo de movimiento
+         * tiene parámetros que no siempre son coincidentes
+         * SOLUCIÓN: Factory + Builder, de forma que:
+         * - Factory decide el tipo a crear
+         * - Builder gestiona los parámetros necesarios para crear dicho objeto, logrando que no se pasen atributos
+         * nulos que no van a usarse dependiendo del tipo de objeto creado
+         */
+        public static Movement createMovement(String type,
+                                              String description,
+                                              Long amount,
+                                              Account destination,
+                                              Movement originalMovement){
+            return switch(type.toUpperCase()){
+                case "DEPOSIT" -> new DepositMovement(UUID.randomUUID().toString(), description, amount);
+                case "WITHDRAWAL" -> new WithdrawalMovement(UUID.randomUUID().toString(), description, amount);
+                case "TRANSFER" -> new TransferMovement(UUID.randomUUID().toString(), description, destination, amount);
+                case "ANNULMENT" -> new Annulment(UUID.randomUUID().toString(), description, originalMovement);
+                default -> throw new UnsupportedOperationException("Unknown movement type");
+            };
+        }
+    }
+
     @Data
     @RequiredArgsConstructor
     public static class Account {
-        public static final ConcurrentHashMap<String,Movement> MOVEMENTS = new ConcurrentHashMap<>();
-        final String id;
-        Long balance = 0L;
+        public static final Map<String, Movement> MOVEMENTS = new ConcurrentHashMap<>();
+        private final String id;
+        private Long balance = 0L;
+
         public void addMovement(Movement movement) {
-            MOVEMENTS.put(movement.getId(),movement);
-            if (Movement.MovementType.DEPOSIT.equals(movement.getType())) {
-                balance += movement.getAmount();
-            } else {
-                balance -= movement.getAmount();
-            }
+            MOVEMENTS.put(movement.getId(), movement);
+            movement.apply(this);
             log.info("Current balance: {}",balance);
         }
     }
     @Test
-    public void test() {
-        Account account = new Account(UUID.randomUUID().toString());
-        account.addMovement(Movement.builder().id("1").type(Movement.MovementType.DEPOSIT).amount(1000L).description("INGRESO").build());
-        account.addMovement(Movement.builder().id("1").type(Movement.MovementType.WITHDRAWAL).amount(10L).description("GASTOS VARIOS").build());
+    void test() {
+        // ...
     }
-    /* TODO
-        Made the refactor to create new movement types, and avoid scalability issues applying the proper creational pattern.
-        Remember, our code MUST follow SOLID Principles, so refactor the classes you need to accomplish it
-     */
-
 
 }
